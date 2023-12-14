6e5528773b7d70faf1a0c73965c3c37df7359875
swooboo <swooboo@mail.ru>
2023-12-15 00:30:43 +0200

    Switch to ArrayList from LinkedList

--- src/main/java/com/swooboo/chatgame/openai/OpenAiControllerImpl.java
+++ src/main/java/com/swooboo/chatgame/openai/OpenAiControllerImpl.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.completion.chat.ChatMessageRole;
 import org.springframework.stereotype.Component;
 import com.theokanning.openai.service.OpenAiService;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class OpenAiControllerImpl implements OpenAiController {
 
     private final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
-    private final List<ChatMessage> messages = new LinkedList<>();
+    private final List<ChatMessage> messages = new ArrayList<>();
     private final OpenAiService openAiService = new OpenAiService(OPENAI_API_KEY);
 
     @Override
